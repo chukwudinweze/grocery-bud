331e4722500f0ae7a7b7584752492eb6728723a1
Chukwudi Nweze <chukwudinwez@gmail.com>
2021-03-24 19:11:00 +0100

    modified and updated App, List and Alert component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,89 @@
-import React, { useState, useEffect } from 'react'
-import List from './List'
-import Alert from './Alert'
+import React, { useState, useEffect } from "react";
+import List from "./List";
+import Alert from "./Alert";
 
 function App() {
-  return <h2>grocery bud setup</h2>
+  // default state values
+  const [name, setName] = useState("");
+  const [list, setList] = useState([]);
+  const [isEditing, setIsEditing] = useState(false);
+  const [editId, setEditId] = useState(null);
+  const [alert, setAlert] = useState({ show: false, msg: "", type: "" });
+
+  // alert function which updates the alert properties depending on where it's being called
+  const showAlert = (show = false, msg = "", type = "") => {
+    setAlert({ show, msg, type });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!name) {
+      showAlert(true, "please enter a value", "danger");
+    } else if (name && isEditing) {
+      setList(
+        list.map((listItem) => {
+          if (listItem.id === editId) {
+            return { ...listItem, title: name };
+          }
+          return listItem;
+        })
+      );
+      setIsEditing(false);
+      setName("");
+      setEditId(null);
+      showAlert(true, "value changed", "success");
+    } else {
+      showAlert(true, "item added to the list", "success");
+      const newItem = { id: new Date().getTime().toString(), title: name };
+      setList([...list, newItem]);
+      setName("");
+    }
+  };
+
+  const clearList = () => {
+    setList([]);
+  };
+
+  const deleteItem = (id) => {
+    showAlert(true, "item removed", "danger");
+    const newList = list.filter((listItem) => listItem.id !== id);
+    setList(newList);
+  };
+
+  const editItem = (id) => {
+    const specificItem = list.find((listItem) => listItem.id === id);
+    setIsEditing(true);
+    setEditId(id);
+    setName(specificItem.title);
+  };
+  return (
+    <section className="section-center">
+      <form className="grocery-form" onSubmit={handleSubmit}>
+        {alert.show && <Alert {...alert} removeALert={showAlert} list={list} />}
+        <h3>grocery bud</h3>
+        <div className="form-control">
+          <input
+            type="text"
+            className="grocery"
+            placeholder="e.g. eggs"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+          <button type="submit" className="submit-btn">
+            {isEditing ? "edit" : "submit"}
+          </button>
+        </div>
+      </form>
+      {list.length > 0 && (
+        <div className="grocery-container">
+          <List listItems={list} deleteItem={deleteItem} editItem={editItem} />
+          <button className="clear-btn" onClick={clearList}>
+            Clear items
+          </button>
+        </div>
+      )}
+    </section>
+  );
 }
 
-export default App
+export default App;
